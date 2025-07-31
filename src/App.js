ad92415bcfd0b2b6bf6e52ef31ecbcf455789f9f
Lena <lenatsivunina@gmail.com>
2025-07-31 21:32:02 +1000

    upd result color, event handler

--- src/App.js
+++ src/App.js
@@ -8,55 +8,60 @@ export const App = () => {
     const operators = ['+', '-', '=', 'C'];
     const [inputValue, setInputValue] = useState('');
     const [operator, setOperator] = useState('');
-    const [calc, newCalc] = useState('');
-    const [sum, newSum] = useState(0);
+    const [calc, setCalc] = useState('');
+    const [sum, setSum] = useState(0);
+    const [isResult, setIsResult] = useState(false);
 
-    const handleInput = (event) => {
-        const { target } = event;
-        const newValue = target.textContent;
+    const handleInput = (newValue) => {
         const isOperator = operators.some((operator) => {
             return operator === newValue;
         });
         const isArithmeticSign = newValue === '+' || newValue === '-';
 
+        if (newValue === '=') {
+            setIsResult(true);
+        } else {
+            setIsResult(false);
+        }
+
         if (!isOperator) {
             setInputValue(inputValue + newValue);
-            newCalc(calc + String(newValue));
+            setCalc(calc + String(newValue));
         } else if (isArithmeticSign) {
-            if (newValue === '+') {
-                setOperator('+');
-                newSum(sum + Number(calc));
-                console.log(sum);
-                newCalc('');
-                setInputValue(inputValue + newValue);
-            } else if (newValue === '-') {
-                setOperator('-');
-                if (sum === 0) {
-                    newSum(Number(calc));
-                    console.log('ok');
-                } else {
-                    newSum(sum - Number(calc));
+            if (inputValue.slice(-1) === '-' || inputValue.slice(-1) === '+') {
+            } else {
+                if (newValue === '+') {
+                    setOperator('+');
+                    setSum(sum + Number(calc));
+                    setCalc('');
+                    setInputValue(inputValue + newValue);
+                } else if (newValue === '-') {
+                    setOperator('-');
+                    if (sum === 0) {
+                        setSum(Number(calc));
+                    } else {
+                        setSum(sum - Number(calc));
+                    }
+                    setCalc('');
+                    setInputValue(inputValue + newValue);
                 }
-                console.log(sum);
-                newCalc('');
-                setInputValue(inputValue + newValue);
             }
         } else if (newValue === '=') {
             if (operator === '+') {
                 const quickSum = sum + Number(calc);
                 setInputValue(quickSum);
-                newCalc('');
-                newSum(quickSum);
+                setCalc('');
+                setSum(quickSum);
             } else if (operator === '-') {
                 const quickSum = sum - Number(calc);
                 setInputValue(quickSum);
-                newCalc('');
-                newSum(quickSum);
+                setCalc('');
+                setSum(quickSum);
             }
         } else if (newValue === 'C') {
             setInputValue('');
-            newCalc(0);
-            newSum(0);
+            setCalc(0);
+            setSum(0);
         }
     };
 
@@ -64,7 +69,7 @@ export const App = () => {
         <div className={styles['app-container']}>
             <div className={styles['input-container']}>
                 <input
-                    className={styles['input-container__input']}
+                    className={`${styles['input-container__input']} ${isResult ? styles['input-container__input--result'] : styles['input-container__input--default']}`}
                     readOnly
                     value={inputValue}
                 ></input>
@@ -79,7 +84,10 @@ export const App = () => {
                                 : styles['numbers-container__item']
                         }
                     >
-                        <button className={styles['buttons']} onClick={handleInput}>
+                        <button
+                            className={styles['buttons']}
+                            onClick={() => handleInput(number)}
+                        >
                             {number}
                         </button>
                     </div>
@@ -88,7 +96,10 @@ export const App = () => {
             <div className={styles['operators-container']}>
                 {operators.map((operator, index) => (
                     <div key={index} className={styles.operator}>
-                        <button onClick={handleInput} className={styles.buttons}>
+                        <button
+                            onClick={() => handleInput(operator)}
+                            className={styles.buttons}
+                        >
                             {operator}
                         </button>
                     </div>
